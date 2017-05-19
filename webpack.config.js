ef3696881f19d400b7596a3aba7527022922062c
Sagar Routh <sagarrouth0512@gmail.com>
2017-05-19 16:49:02 +0530

    Remove html-webpack-plugin because of its incompatibility with devserver, make changes to package.json and index.html accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const config = {
+	context: __dirname,
 	entry: './components/App.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'app.bundle.js'
 	},
+	devServer: {
+    publicPath: '/dist/',
+    historyApiFallback: true,
+		port: 9001
+  },
 	module: {
 		rules: [
 			{include: path.resolve(__dirname, 'components'), test: /\.(js)$/, use: 'babel-loader'},
 			{test: /\.(css)$/, use: [ 'style-loader', { loader: 'css-loader', options: { url: false } }]}
 		]
-	},
-	plugins: [
-		new HtmlWebPackPlugin({template: 'index.html'})
-	]
+	}
 }
 
 module.exports = config
