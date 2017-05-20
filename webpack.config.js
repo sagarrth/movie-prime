b4e5469c6e6b3156990318803551d966d3ef799d
Sagar Routh <sagarrouth0512@gmail.com>
2017-05-20 11:35:28 +0530

    Change the path of bundle file to public folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ const config = {
 	context: __dirname,
 	entry: './components/App.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'public'),
 		filename: 'app.bundle.js'
 	},
 	devServer: {
-    publicPath: '/dist/',
-    historyApiFallback: true,
+		publicPath: '/public/',
+	    historyApiFallback: true,
 		port: 9001
-  },
+  	},
 	module: {
 		rules: [
 			{include: path.resolve(__dirname, 'components'), test: /\.(js)$/, use: 'babel-loader'},
