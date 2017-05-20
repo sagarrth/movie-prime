080155891de5f163746a27c05ee3411654f4913e
Sagar Routh <sagarrouth0512@gmail.com>
2017-05-20 19:52:29 +0530

    Add ratings to be added on Details page using axios from omdbAPI

--- components/Details.js
+++ components/Details.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import Header from './Header'
-import { shape, string } from 'prop-types'
+import React from 'react';
+import Header from './Header';
+import { shape, string } from 'prop-types';
+import axios from 'axios';
 
 class Details extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      omdbData: {}
+    }
+  }
+
+  componentDidMount () {
+    axios.get(`http://www.omdbapi.com/?i=${this.props.movie.imdbID}&apikey=8a93d27f&`)
+    .then(response => {
+      this.setState({
+        omdbData: response.data
+      })
+    })
+    .catch(err => console.error('axios error', err));
+  }  
+
   render () {
     const { title, description, year, poster, trailer } = this.props.movie
+    let rating
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3>{this.state.omdbData.imdbRating}</h3>
+    } else {
+      rating = <img src='/public/img/loading.png' alt='loading image' />
+    }
     return (
       <div className='details'>
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {rating}
           <img src={`/public/img/posters/${poster}`} />
           <p>{description}</p>
         </section>
@@ -30,6 +55,6 @@ Details.propTypes = {
 		trailer: string,
 		description: string
 	})
-}
+};
 
 export default Details
